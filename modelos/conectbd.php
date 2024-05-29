2298c999376d3b84067879454d8b218eff1093ed
MarcUgass <93116190+MarcUgass@users.noreply.github.com>
2024-05-29 18:15:53 +0200

    Add files via upload

--- modelos/conectbd.php
+++ modelos/conectbd.php
@@ -1,35 +1,14 @@
 <?php
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$gender = $_POST['gender'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$number = $_POST['number'];
 
-$serverName = "localhost";
-$userName = "root";
-$userPassword = "";
-$dbname = "proyectoTW";
+require_once('definiciones.php');
+function conectarBD() {
+    $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if ($connection->connect_error) {
+     die("Connection failed: " . $connection->connect_error);
+    }
+    echo "Connected successfully";
 
-//create connection
-
-$connection = new mysqli($serverName, $userName, $userPassword, $dbname);
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-} else {
-    $stmt = $connection->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
-    $stmt->execute();
-    echo"Data inserted successfully";
-    $stmt->close();
-    $connection->close();
+    return $connection;
 }
-
-//$query1 = "insert into registration (firstName, lastName, gender, email, password, number) 
-  //           VALUES ('$firstName', '$lastName', '$gender', '$email', '$password', '$number')";
- // $res1 = mysqli_query($db, $query1);
-echo "Connected successfully";
-
-
 
 ?>
