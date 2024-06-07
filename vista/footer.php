ed8963194013569ccc32166361bbc43f1494f483
MarcUgass <93116190+MarcUgass@users.noreply.github.com>
2024-06-07 20:49:41 +0200

    Delete vista directory

--- vista/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>&copy; 2023 Hotel XYZ. Todos los derechos reservados.</p>
-    <p>Autor: [Tu Nombre]</p>
-    <p><a href="documentacion.pdf">Documentación del Proyecto</a></p>
-</footer>
