308247ca1ac830efb6a7a22863a5c8a53b3becf4
MarcUgass <93116190+MarcUgass@users.noreply.github.com>
2024-07-10 14:26:16 +0200

    Delete vista directory

--- vista/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>&copy; Hotel M&A. Todos los derechos reservados.</p>
-    <p>Autores: Marc y Andrés</p>
-    <p><a href="documentacion.pdf">Documentación del Proyecto</a></p>
-</footer>
